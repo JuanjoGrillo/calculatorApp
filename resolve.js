edfd2f0bcaba4a487750916e79ece0f2b50afd5b
juan <juanjogrillo.it@gmail.com>
2023-08-26 01:11:35 -0300

    Almoust complete the algorithm

---
+++ resolve.js
@@ -0,0 +1,142 @@
+let complexExpression = "((18 * 3 + 8) / 19 - 32)²² * √(13 * (15 + 9)) + √6²"
+let squareExpression = ['1', '+', '5', '+', '4', '-', '(', '√', '3', '*', '7','+', '8', ')', '+', '√', '2', '+', '4']
+let basicOperationsExpression = '1 + ( 4 / 2 ) + ( 3 * ( 2 - 1 ) ) / 2'
+
+function compressExpression (expression) {
+    let compressedExpression = expression.split('').filter(x=>x!==' ')
+    let sectionOfNumbers = ''
+    let newExpression = []
+    let num = 0
+    compressedExpression.forEach(element => {
+        num++
+        if(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].some(num => num === element)) {
+            sectionOfNumbers += element    
+            if (num === compressedExpression.length) {
+                newExpression.push(sectionOfNumbers)
+            }
+        } else {
+            if(sectionOfNumbers !== '') {
+                newExpression.push(sectionOfNumbers)
+                newExpression.push(element)
+                sectionOfNumbers = ''
+            } else {
+                newExpression.push(element)
+            }
+        }
+    })
+    return newExpression
+}
+function resolveExpression (expression) {
+    // let parenthesisChecked = checkForParenthesis(compressExpression(expression))
+    // console.log(parenthesisChecked)
+    // let rootChecked = checkForRoots(parenthesisChecked)
+    // console.log(rootChecked)
+    // return resolveOperations(rootChecked)
+    return resolveOperations(checkForRoots(checkForParenthesis(compressExpression(expression))))
+}
+function checkForParenthesis (expression) {
+    let numberOfParenthesis = expression.filter(chart => chart === '(').length
+    for (let i = 0; i < numberOfParenthesis; i++) {
+        let firstParenthesis = expression.slice(expression.lastIndexOf('('))
+        expression.splice(expression.lastIndexOf('('), firstParenthesis.indexOf(')') + 1, resolveParenthesis(expression.slice(expression.lastIndexOf('('), expression.lastIndexOf('(') + firstParenthesis.indexOf(')')+1)))
+    }
+    return expression
+}
+function resolveParenthesis (parenthesis) {
+    parenthesis.splice(0, 1)
+    parenthesis.splice(-1, 1)
+    return resolveOperations(checkForRoots(parenthesis))
+}
+function checkForRoots (expression) {
+    const numberOfRoots = expression.filter(chart => chart === '√').length
+    for (let i = 0; i < numberOfRoots; i++) {
+        let lastRoot = expression.slice(expression.lastIndexOf('√'))
+        let root = []
+        lastRoot.forEach(chart => {
+            root.push(chart)
+        })
+        expression.splice(expression.lastIndexOf('√'), root.length + 1, resolveRoot(resolveOperations(root.slice(1))))
+    }
+    return expression
+}
+function resolveRoot (expression) {
+    return Math.sqrt(expression).toString()
+}
+function resolveOperations (basicExpression) {
+    let num = 0
+    basicExpression.push(' ')
+    let termTwo = ''
+    let operation = ''
+    let result = ''
+    basicExpression.forEach(chart => {
+        num++
+        if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].some(num => chart.includes(num))) {
+            if(operation === '') {
+                result = parseInt(chart)
+            } else {
+                termTwo += chart
+            }            
+        } else if (chart === '²') {
+            result **= 2
+        } else {
+            if(termTwo !== '') {
+                if (operation === '+') {
+                    result += parseInt(termTwo)
+                } else if (operation === '-') {
+                    result -= parseInt(termTwo)
+                } else if (operation === '*') {
+                    result *= parseInt(termTwo)
+                } else if (operation === '/') {
+                    result /= parseInt(termTwo)
+                }
+            }
+            operation = chart
+            termTwo = ''
+        }
+    })
+    return result.toString()
+}
+let anotherExpression = ['3', '*', '10', '+', '15', '/', '3', '-', '10']
+function separateInTerms (expression) {
+    let terms = []
+    let numberOfTerms = expression.filter(chart=>chart === '+' || chart === '-').length + 1
+    for (let i = 0; i < numberOfTerms; i++) {
+        let indexOperation = []
+        if (expression.indexOf('+')) {
+            if (expression.indexOf('-')) {
+                if (expression.indexOf('+') < expression.indexOf('-')) {
+                    indexOperation = [expression.indexOf('+'), '+']
+                } else {
+                    indexOperation = [expression.indexOf('-'), '-']
+                }
+            } else {
+                indexOperation = [expression.indexOf('+'), '+']
+            }
+        } else {
+            if (expression.indexOf('-')) {
+                indexOperation = [expression.indexOf('-'), '-']
+            }
+        }
+        if ( indexOperation.length !== 0) {
+            let term = expression.splice(0, indexOperation[0] + 1)
+            term.splice(term.length - 1, 1)
+            terms.push(resolveOperations(term))
+            terms.push(indexOperation[1])
+        } else {
+            terms.push(resolveOperations(expression.splice(0, expression.length - 1)))
+        }
+    }
+    return terms
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -4,82 +4,122 @@ class Screen {
     constructor(screenElement){
         this.screenElement = screenElement
     }
+    screenData = []
     inputChart = (e) => {
         if(e.target.classList.contains('number')) {
             this.screenElement.textContent += e.target.textContent
+            this.screenData.push(parseFloat(e.target.textContent))
         } else if(e.target.classList.contains('operation')) {
             if(numbers.some( num => this.screenElement.textContent.slice(-1) === num)) {
                 this.screenElement.textContent += e.target.textContent
-            } else {
-                console.log('Need a number first')
-            }
+                this.screenData.push(e.target.textContent)
+            } 
         } else if(e.target.classList.contains('equal')){
             this.calculateValue()
+        } else if(e.target.classList.contains('openParenthesis')) {
+            this.screenElement.textContent += e.target.textContent
+            this.screenData.push(e.target.textContent)
+        } else if(e.target.classList.contains('closeParenthesis') && numbers.some( num => this.screenElement.textContent.slice(-1) === num)) {
+            this.screenElement.textContent += e.target.textContent
+            this.screenData.push(e.target.textContent)
+        } else if(e.target.classList.contains('squareRoot')) {
+            this.screenElement.textContent += e.target.textContent
+            this.screenData.push(e.target.textContent)
+        } else if(e.target.classList.contains('squarePower') && (numbers.some( num => this.screenElement.textContent.slice(-1) === num) || this.screenElement.textContent.slice(-1) === ")" || this.screenElement.textContent.slice(-1) === "²")) {
+            this.screenElement.textContent = `${this.screenElement.textContent}${'\u00B2'}` 
+            this.screenData.push(e.target.textContent)
+        }
+    }
+    resolveParenthesis = ( chart ) => {
+        const indexOfOpenParenthesis = this.screenElement.textContent.indexOf(chart)
+        let oldScreenText = this.screenElement.textContent
+        const newScreenText = oldScreenText.slice(indexOfOpenParenthesis + 1, oldScreenText.length)
+        
+        if(!newScreenText.includes('(')) {
+            const indexOfCloseParenthesis = this.screenElement.textContent.indexOf(')')
+            console.log(oldScreenText.slice(indexOfOpenParenthesis + 1, indexOfCloseParenthesis))
+        } else {
+            console.log('El texto tiene muchos parentesis')
         }
     }
     calculateValue = () => {
-        const regDelimitationsByAddAndSustract = /([+-])+/
-        let outerTerms = this.screenElement.textContent.split(regDelimitationsByAddAndSustract)
-        if(outerTerms.length === 1) {
-            const regDelimitationsByMultiplyAndDivide = /([*/])+/
-            outerTerms = outerTerms[0].split(regDelimitationsByMultiplyAndDivide)
-            console.log(outerTerms)
-        }
-        // Separar en funciones cada calculo para los casos en que no haya sumas ni restas
-        // Agregar el resto de las funcionalidades con parentesis, raices y potencias
-        while(outerTerms.length !== 1) {
-            if(outerTerms[1] === "+") {
-            const regDelimitationsByMultiplyAndDivide = /([*/])+/
-                const firstInnerTerms = outerTerms[0].split(regDelimitationsByMultiplyAndDivide)
-                const secondInnerTerms = outerTerms[2].split(regDelimitationsByMultiplyAndDivide) 
-                while(firstInnerTerms.length !== 1) {
-                    if(firstInnerTerms[1] === "*") {
-                        const result = parseFloat(firstInnerTerms[0]) * parseFloat(firstInnerTerms[2])    
-                        firstInnerTerms.splice(0, 3, result)
-                    } 
-                    else {
-                        const result = parseFloat(firstInnerTerms[0]) / parseFloat(firstInnerTerms[2])    
-                        firstInnerTerms.splice(0, 3, result)
-                    }
-                }
-                while(secondInnerTerms.length !== 1) {
-                    if(secondInnerTerms[1] === "*") {
-                        const result = parseFloat(secondInnerTerms[0]) * parseFloat(secondInnerTerms[2])    
-                        secondInnerTerms.splice(0, 3, result)
-                    }
-                     else {
-                        const result = parseFloat(secondInnerTerms[0]) / parseFloat(secondInnerTerms[2])    
-                        secondInnerTerms.splice(0, 3, result)
-                    }
-                }
-                const result = parseFloat(firstInnerTerms[0]) + parseFloat(secondInnerTerms[0])
-                console.log("Result = " + result) 
-                outerTerms.splice(0, 3, result)
-            } else {
-                while(firstInnerTerms !== 1) {
-                    if(firstInnerTerms[1] === "*") {
-                        const result = parseFloat(firstInnerTerms[0]) * parseFloat(firstInnerTerms[2])    
-                        firstInnerTerms.splice(0, 3, result)
-                    } else {
-                        const result = parseFloat(firstInnerTerms[0]) / parseFloat(firstInnerTerms[2])    
-                        firstInnerTerms.splice(0, 3, result)
-                    }
-                }
-                while(secondInnerTerms !== 1) {
-                    if(secondInnerTerms[1] === "*") {
-                        const result = parseFloat(secondInnerTerms[0]) * parseFloat(secondInnerTerms[2])    
-                        secondInnerTerms.splice(0, 3, result)
-                    } else {
-                        const result = parseFloat(secondInnerTerms[0]) / parseFloat(secondInnerTerms[2])    
-                        secondInnerTerms.splice(0, 3, result)
-                    }
-                }
-                const result = parseFloat(outerTerms[0]) - parseFloat(outerTerms[2])
-                console.log("Result = " + result)
-                outerTerms.splice(0, 3, result)
+        this.screenData.map( chart => {
+            if (chart === '(') {
+                this.resolveParenthesis(chart)
             }
-        }
-        this.screenElement.textContent = outerTerms[0]
+        })
+        // let indexOfOpenParenthesis = []
+        // let indexOfCloseParenthesis = []
+        // this.screenData.map( chart => {
+        //     if(chart === '(') {
+        //         indexOfOpenParenthesis.push(this.screenData.indexOf(chart))
+        //     } else if(chart === ')') {
+        //         indexOfCloseParenthesis.push(this.screenData.indexOf(chart))
+        //     }
+        // })
+        // for(let h = 0; h < indexOfOpenParenthesis.length; h++){
+        //     for(let i = 0; i < indexOfCloseParenthesis[h]; i++) {
+        //         let parenthesisContent = []
+        //         parenthesisContent.push() 
+        //     }
+        // }
+
+
+    //     const regDelimitationsByAddAndSustract = /([+-])+/
+    //     let outerTerms = this.screenElement.textContent.split(regDelimitationsByAddAndSustract)
+    //     while(outerTerms.length !== 1) {
+    //         if(outerTerms[1] === "+") {
+    //         const regDelimitationsByMultiplyAndDivide = /([*/])+/
+    //             const firstInnerTerms = outerTerms[0].split(regDelimitationsByMultiplyAndDivide)
+    //             const secondInnerTerms = outerTerms[2].split(regDelimitationsByMultiplyAndDivide) 
+    //             while(firstInnerTerms.length !== 1) {
+    //                 if(firstInnerTerms[1] === "*") {
+    //                     const result = parseFloat(firstInnerTerms[0]) * parseFloat(firstInnerTerms[2])    
+    //                     firstInnerTerms.splice(0, 3, result)
+    //                 } 
+    //                 else {
+    //                     const result = parseFloat(firstInnerTerms[0]) / parseFloat(firstInnerTerms[2])    
+    //                     firstInnerTerms.splice(0, 3, result)
+    //                 }
+    //             }
+    //             while(secondInnerTerms.length !== 1) {
+    //                 if(secondInnerTerms[1] === "*") {
+    //                     const result = parseFloat(secondInnerTerms[0]) * parseFloat(secondInnerTerms[2])    
+    //                     secondInnerTerms.splice(0, 3, result)
+    //                 }
+    //                  else {
+    //                     const result = parseFloat(secondInnerTerms[0]) / parseFloat(secondInnerTerms[2])    
+    //                     secondInnerTerms.splice(0, 3, result)
+    //                 }
+    //             }
+    //             const result = parseFloat(firstInnerTerms[0]) + parseFloat(secondInnerTerms[0])
+    //             console.log("Result = " + result) 
+    //             outerTerms.splice(0, 3, result)
+    //         } else {
+    //             while(firstInnerTerms !== 1) {
+    //                 if(firstInnerTerms[1] === "*") {
+    //                     const result = parseFloat(firstInnerTerms[0]) * parseFloat(firstInnerTerms[2])    
+    //                     firstInnerTerms.splice(0, 3, result)
+    //                 } else {
+    //                     const result = parseFloat(firstInnerTerms[0]) / parseFloat(firstInnerTerms[2])    
+    //                     firstInnerTerms.splice(0, 3, result)
+    //                 }
+    //             }
+    //             while(secondInnerTerms !== 1) {
+    //                 if(secondInnerTerms[1] === "*") {
+    //                     const result = parseFloat(secondInnerTerms[0]) * parseFloat(secondInnerTerms[2])    
+    //                     secondInnerTerms.splice(0, 3, result)
+    //                 } else {
+    //                     const result = parseFloat(secondInnerTerms[0]) / parseFloat(secondInnerTerms[2])    
+    //                     secondInnerTerms.splice(0, 3, result)
+    //                 }
+    //             }
+    //             const result = parseFloat(outerTerms[0]) - parseFloat(outerTerms[2])
+    //             console.log("Result = " + result)
+    //             outerTerms.splice(0, 3, result)
+    //         }
+    //     }
+    //     this.screenElement.textContent = outerTerms[0]
     }
 }
 
